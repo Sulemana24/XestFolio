3cf65bd7d543ebee0069a09f6da5aed3877dbd61
Iddrisu_Sulemana <iddrisusulemana665@gmail.com>
2025-03-01 14:43:37 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const menuBtn = document.getElementById("menu-line");
+const navLinks = document.getElementById("ul-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("translate-y-0");
+    navLinks.classList.toggle("-translate-y-full");
+
+    const isOpen = navLinks.classList.contains("translate-y-0");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", () => {
+    navLinks.classList.add("-translate-y-full");
+    navLinks.classList.remove("translate-y-0");
+    menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
